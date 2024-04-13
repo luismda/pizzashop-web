8a9621438a3003ea31b0f211c7d6f7b8f2c5188f
Luís Miguel <luismigueldutraalves@gmail.com>
2024-04-13 17:31:53 -0300

    feat: create header in app layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import './styles/global.css'
 import { Helmet, HelmetProvider } from 'react-helmet-async'
 import { RouterProvider } from 'react-router-dom'
 
+import { ThemeProvider } from './components/theme/theme-provider'
 import { Toaster } from './components/ui/sonner'
 import { router } from './router'
 
 export function App() {
   return (
     <HelmetProvider>
-      <Helmet titleTemplate="%s | pizza.shop" />
-      <RouterProvider router={router} />
-      <Toaster richColors />
+      <ThemeProvider storageKey="pizza-shop-theme" defaultTheme="dark">
+        <Helmet titleTemplate="%s | pizza.shop" />
+        <RouterProvider router={router} />
+        <Toaster richColors />
+      </ThemeProvider>
     </HelmetProvider>
   )
 }
