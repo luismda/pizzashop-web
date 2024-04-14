6a66bbf3c3a754acd75f70eabbb2be60152e656c
Luís Miguel <luismigueldutraalves@gmail.com>
2024-04-14 17:15:59 -0300

    feat: create dashboard screen

---
+++ src/pages/app/dashboard/revenue-chart.tsx
@@ -0,0 +1,79 @@
+import {
+  CartesianGrid,
+  Line,
+  LineChart,
+  ResponsiveContainer,
+  XAxis,
+  YAxis,
+} from 'recharts'
+import colors from 'tailwindcss/colors'
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+
+const data = [
+  { date: '08/04', revenue: 1020 },
+  { date: '09/04', revenue: 680 },
+  { date: '10/04', revenue: 1090 },
+  { date: '11/04', revenue: 890 },
+  { date: '12/04', revenue: 1892 },
+  { date: '13/04', revenue: 3400 },
+  { date: '14/04', revenue: 1210 },
+]
+
+export function RevenueChart() {
+  return (
+    <Card className="col-span-6">
+      <CardHeader className="flex-row items-center justify-between pb-8">
+        <div className="space-y-1">
+          <CardTitle className="text-base font-medium">
+            Receita no período
+          </CardTitle>
+
+          <CardDescription>Receita diária no período</CardDescription>
+        </div>
+      </CardHeader>
+
+      <CardContent>
+        <ResponsiveContainer width="100%" height={240}>
+          <LineChart data={data} style={{ fontSize: 12 }}>
+            <XAxis
+              dataKey="date"
+              stroke="#888"
+              tickLine={false}
+              axisLine={false}
+              dy={16}
+            />
+
+            <YAxis
+              width={80}
+              stroke="#888"
+              axisLine={false}
+              tickLine={false}
+              tickFormatter={(value: number) =>
+                value.toLocaleString('pt-BR', {
+                  style: 'currency',
+                  currency: 'BRL',
+                })
+              }
+            />
+
+            <CartesianGrid vertical={false} className="stroke-muted" />
+
+            <Line
+              type="linear"
+              strokeWidth={2}
+              dataKey="revenue"
+              stroke={colors.violet[500]}
+            />
+          </LineChart>
+        </ResponsiveContainer>
+      </CardContent>
+    </Card>
+  )
+}
